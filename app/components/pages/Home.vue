e61e2c16ab55603bb01f8c61c3e6ed491d50b5ae
Anand Chowdhary <anandchowdhary@gmail.com>
2018-03-10 22:17:08 +0100

    Do some request stuff

--- app/components/pages/Home.vue
+++ app/components/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<v-toolbar>
-			<v-toolbar-title>Your Progress</v-toolbar-title>
+			<v-toolbar-title>Welcome</v-toolbar-title>
 			<v-spacer></v-spacer>
 			<v-btn icon>
 				<v-icon>search</v-icon>
@@ -10,5 +10,25 @@
 				<v-icon>more_vert</v-icon>
 			</v-btn>
 		</v-toolbar>
+		<v-container grid-list-md text-xs-center class="mt-5">
+			<v-layout row wrap>
+				<v-flex xs6>
+					<v-avatar size="75" class="orange">
+						<v-icon x-large dark>security</v-icon>
+					</v-avatar>
+					<p class="mt-3">Level 5</p>
+				</v-flex>
+				<v-flex xs6>
+					<v-avatar size="75" class="indigo">
+						<v-icon x-large dark>star</v-icon>
+					</v-avatar>
+					<p class="mt-3">4.3/5</p>
+				</v-flex>
+			</v-layout>
+		</v-container>
 	</div>
 </template>
+
+<style scoped>
+
+</style>
--- app/components/pages/Requests.vue
+++ app/components/pages/Requests.vue
@@ -1,20 +1,21 @@
 <template>
 	<div>
-		<!-- <gmap-map v-bind:options="mapStyle" :center="{lat: 47.609526517418864, lng: -122.13}" :zoom="13" map-type-id="roadmap" class="bg-map" /> -->
-		<v-card>
-			<v-card-media src="https://via.placeholder.com/400x200" height="150px">
+		<gmap-map v-bind:options="mapStyle" :center="{lat: 47.609526517418864, lng: -122.13}" :zoom="13" map-type-id="roadmap" class="bg-map" />
+		<v-card style="background: #fff" ref="card">
+			<v-card-media src="https://via.placeholder.com/400x200" height="125px">
 			</v-card-media>
 			<v-card-title primary-title>
 				<div>
 					<h3 class="mb-3">
-						Hessel Bosma <span color="orange">★★★★☆</span>
+						{{request.name}} <span color="orange">★★★★☆</span>
 					</h3>
-					<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+					<div>Clean before 8 pm tonight</div>
+					<div>16 mins (7 km) away, leave in 15 minutes</div>
 				</div>
 			</v-card-title>
 			<v-card-actions>
-				<v-btn flat color="grey"><v-icon class="mr-2">close</v-icon>Decline</v-btn>
-				<v-btn flat color="orange"><v-icon class="mr-2">check</v-icon>Accept</v-btn>
+				<v-btn flat color="grey" v-on:click="updateRequest"><v-icon class="mr-2">close</v-icon>Decline</v-btn>
+				<v-btn flat color="teal"><v-icon class="mr-2">check</v-icon>Accept for €42</v-btn>
 			</v-card-actions>
 		</v-card>
 	</div>
@@ -28,15 +29,28 @@
 					mapTypeControl: false,
 					zoomControl: false,
 					styles: [ { "elementType": "geometry", "stylers": [ { "color": "#f5f5f5" } ] }, { "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#616161" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#f5f5f5" } ] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#bdbdbd" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#eeeeee" } ] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#757575" } ] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [ { "color": "#d8ebd4" } ] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#ffffff" } ] }, { "featureType": "road.arterial", "elementType": "labels.text.fill", "stylers": [ { "color": "#757575" } ] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [ { "color": "#616161" } ] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "elementType": "geometry", "stylers": [ { "color": "#cedfe0" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#9e9e9e" } ] } ]},
-				form: {
-					email: "",
-					name: "",
-					company: "",
-					message: "",
+				request: {
+					name: "Hessel Bosma"
 				},
 				show: true,
 				sent: false,
 				error: null
+			}
+		},
+		methods: {
+			updateRequest() {
+				this.$refs.card.$el.classList.add("move-enter-active");
+				this.$refs.card.$el.classList.add("move-enter");
+				setTimeout(() => {
+					this.$refs.card.$el.classList.remove("move-enter-active");
+					this.$refs.card.$el.classList.remove("move-enter");
+					this.$refs.card.$el.classList.add("move-leave-active");
+					this.$refs.card.$el.classList.add("move-leave");
+					setTimeout(() => {
+						this.$refs.card.$el.classList.remove("move-leave-active");
+						this.$refs.card.$el.classList.remove("move-leave");
+					}, 300);
+				}, 300);
 			}
 		}
 	}
