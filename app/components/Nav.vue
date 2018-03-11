ce410e7b4ec5ffcdeebe828f5172813bd0f89849
Anand Chowdhary <anandchowdhary@gmail.com>
2018-03-11 06:54:40 +0100

    Fix Mohit's profile

--- app/components/Nav.vue
+++ app/components/Nav.vue
@@ -13,10 +13,6 @@
 				<span>Community</span>
 				<v-icon>question_answer</v-icon>
 			</v-btn>
-			<v-btn to="/inclusion" flat color="teal" value="inclusion">
-				<span>Inclusion</span>
-				<v-icon>accessibility</v-icon>
-			</v-btn>
 			<v-btn to="/Profile" flat color="teal" value="profile">
 				<span>Profile</span>
 				<v-icon>face</v-icon>
